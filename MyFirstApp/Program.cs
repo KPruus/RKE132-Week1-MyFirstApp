cdad581ab1fce18239b8de5f7f322a9224199002
KPruus <kristjanpruus@hotmail.com>
2024-04-21 17:12:39 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See on el commento
+
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Tervitab kasutajat nimepidi
+
+Console.WriteLine("Sisestagem nimi:");
+//String on sõne (reaalne sõna)
+string userName;
+userName = Console.ReadLine();
+Console.WriteLine("Tervist ja jõudu, " + userName);
+
+//String interpolation
+Console.WriteLine($"No Tervist noh, {userName}!");
